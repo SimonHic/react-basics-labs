0df44543bcd28a2a2b9fd5c32108f9810c7558c9
SimonHic <simonhicdev@gmail.com>
2024-10-08 02:21:55 +0100

    Tasky Lab 2 - Exercises Completed

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,23 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
 import AddTaskForm from './components/Form';
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Task from './components/Task'
 
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", done: false}
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium", done: false },
+      { id: 3, title: "Tidy up", description: "Tidy up room for guests", deadline: "Today", priority: "High", done: false}
     ]
   });
 
   const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority: ""
   });
 
   const doneHandler = (taskIndex) => {
@@ -46,6 +46,9 @@ function App() {
       case "deadline":
           form.deadline = event.target.value;
           break;
+      case "priority":
+          form.priority = event.target.value;
+          break;
       default:
           form = formState;
     }
@@ -73,6 +76,7 @@ function App() {
         title={task.title}
         description={task.description}
         deadline={task.deadline}
+        priority={task.priority}
         key={task.id}
         done={task.done}
         markDone={() => doneHandler(index)}
--- tasky/src/components/Form.js
+++ tasky/src/components/Form.js
@@ -19,6 +19,16 @@ const AddTaskForm = (props) => {
         Details:
         <input type="text" name="description" onChange={(event) => props.change(event)} />
     </label>
+    <br />
+    <label>
+        Priority Level:
+        <select name="priority" onChange={(event) => props.change(event)} >
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+          </select>
+          <br />
+    </label>
         <input type="submit" value="Submit" />
         </form>
     </div>
